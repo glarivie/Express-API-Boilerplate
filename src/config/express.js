1035a5e12fc812aa85592191077dbb16866fe972
Guillaume Larivière <guillaume@recast.ai>
2017-02-08 09:46:46 +0100

    [ADD] Helmet middleware for security purpose

--- src/config/express.js
+++ src/config/express.js
@@ -1,6 +1,7 @@
 import cors from 'cors';
 import express from 'express';
 import morgan from 'morgan';
+import helmet from 'helmet';
 import cookieParser from 'cookie-parser';
 import bodyParser from 'body-parser';
 import { log, error } from 'console';
@@ -9,22 +10,17 @@ import router from '../routes/main';
 const app = express();
 const { SERVER_PORT } = process.env;
 
-app.use(morgan('dev'));
-app.use(cookieParser());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app
+  .use(morgan('dev'))
+  .use(cookieParser())
+  .use(bodyParser.json()) // Parse application/json
+  .use(bodyParser.urlencoded({ extended: true })) // Parse application/x-www-form-urlencoded
+  .use(cors()) // Enable Cross Origin Resource Sharing
+  .use(helmet()) // Secure your app by setting various HTTP headers
+  .disable('x-powered-by') // Disable 'X-Powered-By' header in response
+  .disable('etag'); // Remove No Cache Control
 
-// enable CORS - Cross Origin Resource Sharing
-app.use(cors());
-
-// disable 'X-Powered-By' header in response
-app.disable('x-powered-by');
-
-// Remove No Cache Control
-app.disable('etag');
-
-// Routes
-app.use('/', router);
+app.use('/', router); // Routes
 
 // Error handler
 app.use((err, req, res, next) => {
@@ -35,6 +31,6 @@ app.use((err, req, res, next) => {
   return res.status(500).send(error.message)
 });
 
-app.listen(SERVER_PORT, () => {
-  log(`[Express] Api is running on ${SERVER_PORT}`);
-});
+app.listen(SERVER_PORT, () =>
+  log(`[Express] Api is running on port ${SERVER_PORT}`)
+);
