4fa68bbd17e0dc142f5aab59aa35d3cfbc2d1316
Guillaume Larivière <guillaume@recast.ai>
2017-02-08 11:28:01 +0100

    [UPD] /login POST route with JWT authentication

--- src/config/express.js
+++ src/config/express.js
@@ -4,7 +4,9 @@ import morgan from 'morgan';
 import helmet from 'helmet';
 import cookieParser from 'cookie-parser';
 import bodyParser from 'body-parser';
+import passport from 'passport';
 import { log, error } from 'console';
+
 import router from '../routes/main';
 
 const app = express();
@@ -17,6 +19,9 @@ app
   .use(bodyParser.urlencoded({ extended: true })) // Parse application/x-www-form-urlencoded
   .use(cors()) // Enable Cross Origin Resource Sharing
   .use(helmet()) // Secure your app by setting various HTTP headers
+  .use(passport.initialize()); // initialize passport middleware
+
+app
   .disable('x-powered-by') // Disable 'X-Powered-By' header in response
   .disable('etag'); // Remove No Cache Control
 
--- src/routes/main.js
+++ src/routes/main.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import passport from 'passport';
 
 import * as demoController from '../controllers/demo';
 
@@ -9,5 +10,18 @@ router.get('/', (req, res) => {
 });
 
 router.get('/api/demo', demoController.demo);
+
+router.post(
+  '/login',
+  passport.authenticate('jwt', { session: false }),
+  ({ headers, user }, res) => {
+    res.status(200).send({
+      status: 200,
+      response: 'Authorization granted !',
+      headers,
+      user,
+    });
+  },
+)
 
 export default router;
