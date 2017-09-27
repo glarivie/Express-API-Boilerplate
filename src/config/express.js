56411f1f91f30f9a7bae3187717f99f91f9cbed4
Guillaume LARIVIERE <g.lariviere@qwant.com>
2017-09-27 10:53:01 +0200

    [DEL] remove useless cookie-parser

--- src/config/express.js
+++ src/config/express.js
@@ -2,7 +2,6 @@ import cors from 'cors'
 import express from 'express'
 import morgan from 'morgan'
 import helmet from 'helmet'
-import cookieParser from 'cookie-parser'
 import bodyParser from 'body-parser'
 import passport from 'passport'
 import { log } from 'console'
@@ -15,7 +14,6 @@ const app = express()
 
 app
   .use(morgan('dev')) // :method :url :status :response-time ms - :res[content-length]
-  .use(cookieParser())
   .use(bodyParser.json()) // Parse application/json
   .use(bodyParser.urlencoded({ extended: true })) // Parse application/x-www-form-urlencoded
   .use(cors()) // Enable Cross Origin Resource Sharing
@@ -33,3 +31,5 @@ app
 app.listen(SERVER_PORT, () =>
   log('[Express] Api is running on port', SERVER_PORT),
 )
+
+export default app
