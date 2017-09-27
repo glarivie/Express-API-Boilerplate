b89ae1be05afc1deb263f490aca249ce303bd9b4
Guillaume LARIVIERE <g.lariviere@qwant.com>
2017-09-27 10:53:17 +0200

    [UPD] mongoose config

--- src/config/mongo.js
+++ src/config/mongo.js
@@ -1,20 +1,20 @@
 import mongoose from 'mongoose'
-import paginate from 'mongoose-paginate'
 import { log, error } from 'console'
 
 import demoSchema from '../schemas/demoSchema'
 
-mongoose.Promise = Promise
-
 // const { DB_USER, DB_PORT, DB_PASSWORD, DB_HOST, DB_COLLECTION } = process.env
+// const options = {
+//   useMongoClient: true,
+//   promiseLibrary: Promise,
+// }
 
 // Create mongo database connection
-// mongoose.connect(`mongodb://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_COLLECTION}`)
+// mongoose.connect(`mongodb://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_COLLECTION}`, options)
 
 /* eslint-disable no-console */
 mongoose.connection.on('connected', () => log('[MongoDB] Database is  connected'))
 mongoose.connection.on('disconnected', () => error('[MongoDB] Database is disconnected'))
 /* eslint-enable no-console */
 
-demoSchema.plugin(paginate)
 mongoose.model('Demo', demoSchema)
